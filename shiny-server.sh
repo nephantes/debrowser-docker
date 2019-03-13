40a9c2a07f1867bb4871e13e72097283e2b45b87
Alper Kucukural <alper.kucukural@umassmed.edu>
2019-03-12 23:58:29 -0400

    service fix

--- shiny-server.sh
+++ shiny-server.sh
@@ -3,4 +3,5 @@
 # Make sure the directory for individual app logs exists
 mkdir -p /var/log/shiny-server
 chown shiny.shiny /var/log/shiny-server
-exec shiny-server 2>&1 &
+
+exec shiny-server 2>&1 
